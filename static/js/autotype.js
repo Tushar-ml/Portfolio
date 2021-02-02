9d569ac7e75cd1fa67ac04fb5f5b3b087d3bcee5
TusharGoel <tgoel219@gmail.com>
2021-02-03 00:25:05 +0530

    Some UI Changes

--- static/js/autotype.js
+++ static/js/autotype.js
@@ -1,10 +1,10 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Tushar Goel","a Data Scientist.","a Freelancer.","an Artificial Intelligence."];
-const typingDelay = 200;
+const textArray = ["Tushar Goel","a Junior ML Engineer.","a Freelancer.","a Developer."];
+const typingDelay = 10;
 const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
+const newTextDelay = 1000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
